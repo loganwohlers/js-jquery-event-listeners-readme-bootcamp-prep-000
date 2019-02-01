d49037f31dddc5390f4c084cf5bc68727fe55bd3
Logan Wohlers <lawohlers@gmail.com>
2019-02-01 02:23:09 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert ('hey');
+  });
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $( this ).addClass( "tasty" );
+  });
+}
+
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if (key.which==71){
+      alert('you pressed G');
+    }
+  });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  });
+}
+
 
 $(document).ready(function(){
-
-// call functions here
+getIt();
+frameIt();
+pressIt();
 
 });
