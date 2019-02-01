13cfb75abf686f4fd7ce8692f2284fbfa8eed1c7
Logan Wohlers <lawohlers@gmail.com>
2019-02-01 02:17:52 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -7,12 +7,14 @@ function getIt(){
 function frameIt(){
   $('img').on('load', function(){
     $( this ).addClass( "tasty" );
-  })
+  });
 }
 
 
 
 $(document).ready(function(){
 getIt();
+frameIt();
+
 
 });
